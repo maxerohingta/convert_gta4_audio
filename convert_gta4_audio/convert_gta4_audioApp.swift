100d0dec903bdb59eeb457e97160485619a35123
maxerohingta <maxerohin@gmail.com>
2025-07-31 10:35:30 +0300

    init swift

---
+++ convert_gta4_audio/convert_gta4_audioApp.swift
@@ -0,0 +1,17 @@
+//
+//  convert_gta4_audioApp.swift
+//  convert_gta4_audio
+//
+//  Created by Alexey Vorobyov on 31.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct convert_gta4_audioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
